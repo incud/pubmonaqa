977a50556527257290c8398a5352ec2553441c8f
Massimiliano <massimiliano.incudini@univr.it>
2026-05-27 14:10:00 +0200

    fixed launcher testbench

--- timing_estimation/fpga/launch_testbench_local.sh
+++ timing_estimation/fpga/launch_testbench_local.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+set -u -o pipefail
+
+echo "Sourcing..."
+source /scratch/mincudin/xlnx/2025.2/Vitis/settings64.sh
+
 export SK_N_VAL=20
 export F_VAL=64
 export HLS_PART=xcvu19p-fsva3824-2-e
